03563e5bb6269acdf01f5429cf91c1ec6c09d1d0
Golam Zaid <golamzaid101@gmail.com>
2025-08-10 00:17:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# Load the saved model and vectorizer at startup
+clf = joblib.load("NB_spam_model.pkl")
+cv = joblib.load("count_vectorizer.pkl")
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
